28628068edad9f66427a167783156e548719a28f
guddumuslim <dovehunter981@gmail.com>
2026-09-17 16:56:24 +0530

    Fix file mode for loading the model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load the saved model
-model = joblib.load(open('linear.sav', 'rb'))
+model = joblib.load(open('linear.sav', 'lr'))
 
 st.title('Sales Prediction App')
 
